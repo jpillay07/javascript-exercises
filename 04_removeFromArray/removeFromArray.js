fd30487aa896149ce75c7bdd0302228127c1aab9
Justin Pillay <jpillay07@gmail.com>
2022-02-16 16:24:30 +0200

    Exercise 04 completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(theArray) {
+    
 
+    for(let i = 1; i <= arguments.length - 1; i++) { //Loop through all the arguments (items that need to be removed from theArray)
+        let indexToRemove = theArray.indexOf(arguments[i]); // locate the index of the item that needs to be removed
+        if(indexToRemove > -1){// If the item exists, proceed to remove it otherwise do nothing
+            theArray.splice(indexToRemove, 1);
+        }
+    }
+    
+
+    return theArray; // return the edited array
 };
 
 // Do not edit below this line
