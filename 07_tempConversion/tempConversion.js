7de1c4de15ac02829b3a22cfe8a04eb42bc4fbc7
Justin Pillay <jpillay07@gmail.com>
2022-02-17 12:41:26 +0200

    Exercise 07 completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
 const ftoc = function() {
 
+  let farenheit = ((arguments[0] - 32) * (5/9));
+  return Math.round(farenheit * 10) / 10;
+
 };
 
 const ctof = function() {
 
+  let celsius = (arguments[0] * (9/5)) + 32;
+
+  return Math.round(celsius * 10) / 10;
 };
 
 // Do not edit below this line
