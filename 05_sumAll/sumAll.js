accd3e89e3c512e314ac6da8c57d1b4d83d779d9
Justin Pillay <jpillay07@gmail.com>
2022-02-17 11:46:41 +0200

    Exercise 05 completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,42 @@
 const sumAll = function() {
 
+    let sum = 0;
+
+    let biggest = 0;
+
+    let smallest = 0;
+
+    if(!(typeof arguments[0] === "number")){
+        return "ERROR";
+    }
+
+    if(!(typeof arguments[1] === "number")){
+        return "ERROR";
+    }
+
+    if(arguments[0] < 1){
+        return "ERROR";
+    }
+
+    if(arguments[1] < 1){
+        return "ERROR";
+    }
+
+    if(arguments[0] > arguments[1]){
+        biggest = arguments[0];
+        smallest = arguments[1];
+    }
+    else{
+        biggest = arguments[1];
+        smallest = arguments[0];
+    }
+
+    for (let i = smallest; i <= biggest; i++){
+        sum += i;
+    }
+
+    return sum;
+
 };
 
 // Do not edit below this line
